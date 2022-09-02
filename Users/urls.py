07c6f83086eb1ae38c38bd8cd980c821148fd350
PROGRAMMING-ACES <qdubsmusk@gmail.com>
2022-09-02 16:47:25 +0100

    First Commit

---
+++ Users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import LoginView, RegisterView
+
+urlpatterns = [
+    path('login', LoginView, name="login"),
+    path('register', RegisterView, name="register"),
+]
